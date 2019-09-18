c4fb174a35755f7fe27ae4ab4dcb0d283397cf35
Claus Wilke <wilke@austin.utexas.edu>
2019-09-18 16:39:03 -0500

    Imported first example

---
+++ R/corruption.R
@@ -0,0 +1,25 @@
+#' Corruption Perceptions Index (CPI) and Human Development Index (HDI)
+#'
+#' Corruption Perceptions Index (CPI) and Human Development Index (HDI) for 176 countries,
+#' from 2012 to 2015.
+#'
+#' @references
+#' Corruption Perceptions Index: \url{https://www.transparency.org/news/feature/corruption_perceptions_index_2016#table}
+#'
+#' Human Development Index: \url{http://hdr.undp.org/en/data#}
+#'
+#' Original figure idea: \url{https://www.economist.com/blogs/dailychart/2011/12/corruption-and-development}
+#'
+#' @examples
+#' library(tidyverse)
+#' library(cowplot)
+#'
+#' corruption %>%
+#'   filter(year == 2015) %>%
+#'   na.omit() %>%
+#'   ggplot(aes(cpi, hdi)) + geom_point(aes(color = region)) +
+#'   geom_smooth(method = 'lm', formula = y~log(x), se = FALSE) +
+#'   scale_color_brewer(name = NULL, type = "qual") +
+#'   theme_minimal_hgrid() +
+#'   theme(legend.position = "top")
+"corruption"
---
+++ R/practicalgg.R
@@ -0,0 +1,6 @@
+#' Practical ggplot2, a package demonstrating practical uses of the ggplot2 package.
+#'
+#' Practical ggplot2, a package demonstrating practical uses of the ggplot2 package.
+#' @name practicalgg
+#' @docType package
+NULL
