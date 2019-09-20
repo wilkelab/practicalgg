39ab4818eee3b93c6945e4578cc7b98a29160978
Claus Wilke <wilke@austin.utexas.edu>
2019-09-20 10:29:42 -0500

    texas income dataset, enable travis ci

--- R/happy.R
+++ R/happy.R
@@ -2,4 +2,14 @@
 #'
 #' This dataset provides a small sample of variables related to happiness from the general social survey (GSS).
 #' It is a reexport of the [`productplots::happy`] dataset, converted into a tibble.
+#'
+#' @examples
+#' library(tidyverse)
+#'
+#' happy %>%
+#'   na.omit() %>%
+#'   mutate(health = fct_rev(health)) %>%
+#'   ggplot(aes(x = age, y = stat(count))) +
+#'   geom_density(fill = "lightblue") +
+#'   facet_wrap(~health)
 "happy"
---
+++ data-raw/texas_income.R
@@ -0,0 +1,16 @@
+library(tidycensus)
+library(tidyverse)
+
+options(tigris_use_cache = TRUE)
+
+##****************
+## Median income
+##****************
+
+# get median income
+texas_income <- get_acs(
+  state = "TX",  geography = "county", year = 2015,
+  variables = "B19013_001", geometry = TRUE
+)
+
+usethis::use_data(texas_income, overwrite = TRUE)
