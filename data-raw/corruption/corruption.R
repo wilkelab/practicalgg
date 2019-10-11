954564165be875500c987749be5f7677a7c2a5dc
Jesus M. Castagnetto <jesus@castagnetto.com>
2019-10-11 10:08:54 -0500

    Use iso3c for countries to avoid NAs in HDI
    Looking at the dataset, I noticed that some countries had "NA" for the HDI because the names did not match exactly between the two original datasets. This can be fixed if we use the `countrycode` package to map the names to ISO3C codes. In the end the countryes in the `corruption` dataframe that do not have HDIs will be: "Taiwan" , "Kosovo" , "Korea (North)", "Somalia" (which aren't in the original data)
    
    Also, there was a minor encoding issue that affected countries such as "Côte d'Ivoire", that was fixed using the "locale" option of `read_csv()`

--- data-raw/corruption/corruption.R
+++ data-raw/corruption/corruption.R
@@ -2,6 +2,7 @@ library(here)
 library(dplyr)
 library(tidyr)
 library(readr)
+library(countrycode)
 
 df_cpi <- read_delim(
   here("data-raw", "corruption", "CPI_raw.txt"),
@@ -13,7 +14,8 @@ df_cpi %>% select(-`2016 Rank`) %>%
   extract(year, "year", regex = "(.+) Score") %>%
   mutate(
     year = as.numeric(year),
-    score = as.numeric(score)
+    score = as.numeric(score),
+    iso3c = countrycode(Country, "country.name", "iso3c")
   ) %>%
   rename(
     country = Country,
@@ -24,17 +26,22 @@ df_cpi %>% select(-`2016 Rank`) %>%
 
 df_hdi <- read_csv(
   here("data-raw", "corruption", "HDI_raw.csv"),
-  col_types = "icd-d-d-d-d-d-d-d-d-d-d-d-d-d-d-d-d-d-d-d-d-d-d-d-d-d-"
+  col_types = "icd-d-d-d-d-d-d-d-d-d-d-d-d-d-d-d-d-d-d-d-d-d-d-d-d-d-", 
+  locale = locale(encoding = "ISO-8859-1")
 )
 
 df_hdi %>% select(-`HDI Rank (2015)`) %>%
   gather(year, score, `1990`:`2015`) %>%
-  mutate(year = as.numeric(year)) %>%
+  mutate(
+    year = as.numeric(year),
+    iso3c = countrycode(Country, "country.name", "iso3c")
+  ) %>%
   rename(
-    country = Country,
     hdi = score
-  ) -> df_hdi_tidy
+  ) %>% 
+  select(-Country) -> df_hdi_tidy
 
-corruption <- left_join(df_cpi_tidy, df_hdi_tidy, by = c("country", "year")) %>%
+corruption <- left_join(df_cpi_tidy, df_hdi_tidy, by = c("iso3c", "year")) %>%
   filter(year != 2016)
 usethis::use_data(corruption, overwrite = TRUE)
+
