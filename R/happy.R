d493c5743f93dd9c3436d75c5bad1db4d216e62f
Claus Wilke <wilke@austin.utexas.edu>
2021-02-01 16:49:56 -0600

    rebuild docs

--- R/happy.R
+++ R/happy.R
@@ -1,7 +1,7 @@
 #' Data related to happiness from the general social survey
 #'
 #' This dataset provides a small sample of variables related to happiness from the general social survey (GSS).
-#' It is a reexport of the [`productplots::happy`] dataset, converted into a tibble.
+#' It is a reexport of the `productplots::happy` dataset, converted into a tibble.
 #'
 #' @examples
 #' library(tidyverse)
